abad13f212ef8dd15268682b48d55c3bc9f492a9
Miguel Lopes <mpmiguel2007@gmail.com>
2026-06-08 15:26:42 -0300

    feat: Criação do DTOs

---
+++ product-service/shopflow/src/main/java/br/com/shopflow/product/dto/StockDecreaseRequest.java
@@ -0,0 +1,9 @@
+package br.com.shopflow.product.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record StockDecreaseRequest(
+        @NotNull @Min(1) Integer quanttity
+) {
+}
