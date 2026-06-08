29e2d2cabf0d88096172c34e12e2ca5f3bfa69bb
Miguel Lopes <mpmiguel2007@gmail.com>
2026-06-08 15:18:53 -0300

    feat: Criação do entidade 'Product' e da configs gerais do microsserviço

---
+++ product-service/shopflow/src/main/java/br/com/shopflow/product/model/Product.java
@@ -0,0 +1,42 @@
+package br.com.shopflow.product.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(length = 500)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Integer stockQuantity;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
